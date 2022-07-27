409a1d72e9fb70eaec4e37cd7b88d69747208cb3
Abdul-Mateen <matdboi@gmail.com>
2022-07-27 09:24:42 +0100

    go

--- app.R
+++ app.R
@@ -2,7 +2,7 @@
 
 library(shiny)
 library(shinydashboard)
-#library(stringr)
+library(stringr)
 #library(dplyr)
 #library(ggplot2)
 library(shinyWidgets)
@@ -16,13 +16,13 @@ ui <- dashboardPage(
       valueBoxOutput("vBox1", width = 6),
       valueBoxOutput("vBox2", width = 6)
     ),
+    h4("Nairaland users data, as extracted from their respective profile pages [you can verify through the profile links]"),
     fluidRow(
-      box(width = 12, title = "Nairaland users data, as extracted from their respective profile pages [ you can verify through the profile links ;) ]",
-          status = "primary", solidHeader = T, reactableOutput("table"))
+      box(width = 12, status = "primary", solidHeader = F, reactableOutput("table"))
     ),
     div("- The current table was generated from all threads on just the first page of 'https://www.nairaland.com/career'", style = "color:red"),
-    div("- There would be more features added to the app, soon [ e.g. charts ].", style = "color:red"),
-    div("- For all columns with wrapped text, kindly resize the column [ just drag its borders ]", style = "color:red"),
+    div("- There would be more features added to the app, soon [e.g. charts].", style = "color:red"),
+    div("- For all columns with wrapped text, kindly resize the column [drag its borders]", style = "color:red"),
     div("- The data is still in its raw form", style = "color:red")
   )
 )
@@ -42,13 +42,17 @@ server <- function(input, output, session) {
   output$table <- renderReactable({
     reactable(nairaland_users_data, resizable = T, searchable = T, bordered = T, rownames = F,
               filterable = T, showPageSizeOptions = T, highlight = T, outlined = T, striped = T,
-              wrap = F, defaultPageSize = 15, columns = list(personal_text = colDef(width = 150),
-                                                             profile_link = colDef(width = 150),
+              wrap = F, defaultPageSize = 25, columns = list(name = colDef(width = 140),
+                                                             personal_text = colDef(width = 150),
+                                                             profile_link = colDef(width = 300, html = T,
+                                                                                   cell = function(value) str_c("<a href=\"", value, "\">", value, "</a>")),
                                                              time_registered = colDef(width = 150),
                                                              last_seen = colDef(width = 150),
                                                              personal_text = colDef(width = 150),
+                                                             location = colDef(width = 150),
                                                              time_spent_online = colDef(width = 150)),
-              defaultColDef = colDef(na = "-"))
+              defaultColDef = colDef(na = "-", headerStyle = list(background = "steelblue", color = "white"),
+                                     header = function(value) gsub("_", " ", value) %>% str_to_title()))
   })
 }
 
